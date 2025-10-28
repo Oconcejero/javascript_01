9fca214fbf263a71a6380948033ef22a9e83fbd4
Oconcejero <oconcejero@gmail.com>
2025-10-28 16:25:50 +0100

    Cambios 28-10-25 16:25

---
+++ ejer-js/ejer-05.js
@@ -0,0 +1,17 @@
+import '../sass/main.scss';
+
+const textarea = document.querySelector('.parrafo');
+const inputFiltro = document.querySelector('.buscar');
+const resultado = document.querySelector('.resultado');
+
+inputFiltro.addEventListener('input', () => {
+    const textoOriginal = textarea.value;
+    const filtro = inputFiltro.value.toLowerCase();
+    const palabras = textoOriginal.split(" ");
+
+    const coincidencias = palabras.filter(palabra =>
+        palabra.toLowerCase().includes(filtro)
+    );
+
+    resultado.textContent = coincidencias.join(' ');
+});
